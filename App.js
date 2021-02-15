9c56aaf645f5f35c1df546676927b96fb19cf1c9
Mauro Luna <mauroluna.dev@gmail.com>
2021-02-15 13:52:48 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import getTheme from './native-base-theme/components';
+import { Container, Content, Text, StyleProvider } from 'native-base';
+import platform from './native-base-theme/variables/platform';
+
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded, error] = useFonts({ 
+    Roboto: require('./assets/fonts/roboto/Roboto.ttf'),
+    RobotoLight: require('./assets/fonts/roboto/RobotoLight.ttf'),
+    RobotoMedium: require('./assets/fonts/roboto/RobotoMedium.ttf'),
+
+    HelveticaRegular: require('./assets/fonts/helvetica/HelveticaNeueRegular.otf'),
+    HelveticaMedium: require('./assets/fonts/helvetica/HelveticaNeueMedium.otf'),
+    HelveticaLight: require('./assets/fonts/helvetica/HelveticaNeueLight.otf'),
+    HelveticaBold: require('./assets/fonts/helvetica/HelveticaNeueBold.otf'),
+   });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyleProvider style={getTheme(platform)}>
+    <Container>
+      <Content>
+        <Text>
+          I have changed the text color.
+        </Text>
+      </Content>
+    </Container>
+  </StyleProvider>
   );
 }
 
